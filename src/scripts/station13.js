a54705a8a0a1872821b46805221b7aa1eaef1164
ritoserika <89260996+ritoserika@users.noreply.github.com>
2021-09-11 23:20:11 +0900

    station13.html

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
+const arrowFunction =()=>{
+    return "Arrow"
+}
 function getData() {
     // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+    return arrowFunction() // ここにアロー関数の処理を書く
 }
+
+export {getData};
